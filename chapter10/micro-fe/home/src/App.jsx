51931a07eaa776d015376a756a72fbdea7aa3171
Jack Herrington <jherr@pobox.com>
2020-10-05 07:12:57 -0700

    Updates to rc-3

--- chapter10/micro-fe/home/src/App.jsx
+++ chapter10/micro-fe/home/src/App.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
-const Widget = React.lazy(() => import("widget/Widget"));
+// const Widget = React.lazy(() => import("widget/Widget"));
 
 const App = () => (
   <div>
-    <React.Suspense fallback={<div>Loading</div>}>
+    {/* <React.Suspense fallback={<div>Loading</div>}>
       <Widget />
-    </React.Suspense>
+    </React.Suspense> */}
     <div>Hi there, I'm React from React.</div>
   </div>
 );
