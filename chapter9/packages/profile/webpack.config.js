fe9ba68994210b589aa60f319d7281de223a98a0
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-07 19:37:46 -0700

    Add extention resolution for ESM

--- chapter9/packages/profile/webpack.config.js
+++ chapter9/packages/profile/webpack.config.js
@@ -24,6 +24,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js/, type: "javascript/auto",
+        resolve: {
+          fullySpecified: false
+        }
+      },      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
